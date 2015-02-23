5658f7c9031081388eb642872a455ae46ef99adc
Johannes Schickling <schickling.j@gmail.com>
2015-02-23 17:21:37 +0100

    updated deps

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-#![allow(unstable)]
+#![feature(core)]
+#![feature(std_misc)]
+#![feature(old_io)]
 
 extern crate "rustc-serialize" as rustc_serialize;
 extern crate docopt;
@@ -24,7 +26,7 @@ Options:
     -v, --version    Print version info and exit
 ";
 
-#[derive(RustcDecodable, Show)]
+#[derive(RustcDecodable, Debug)]
 struct Args {
     cmd_put: bool,
     arg_message: String,
